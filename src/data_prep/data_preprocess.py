f0e6448c989b3635c74fdf6e7da6b883985c2821
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-02-20 21:35:54 +0000

    Literature review of viable features ongoing

--- src/data_prep/data_preprocess.py
+++ src/data_prep/data_preprocess.py
@@ -33,7 +33,6 @@ case_ids = base_credit_2020[['case_id']]
 prev_app = previous_application_df[cols]
 prev_app_2 = previous_application_df_2[cols]
 
-
 # %%
 bureau_data = pd.read_parquet("data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_credit_bureau_a_1_1.parquet")
 bureau_data
@@ -51,26 +50,30 @@ other_data
 # %%
 static_data = pd.read_parquet("data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_static_0_0.parquet")
 static_data
+
+#%%
+static_data_2 = pd.read_parquet("data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_static_cb_0.parquet")
+static_data_2
+
 # %%
 tax_registry = pd.read_parquet("data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_tax_registry_a_1.parquet")
 tax_registry
 
 # %%
 person_data = pd.read_parquet("data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_person_1.parquet")
-person_data
+person_data = 
 # %%
 
 
 #From literature review, I have selected the following features to build a simple dataset
-# 1. Age
-# 2.Current employment 
+# 1.Age - found in static_data_2 - column name: dateofbirth_337D, dateofbirth_342D
+# 2.Main income amount -  found in static data - column name: maininc_215A & found in person_data - column name: mainoccupationinc_384A & found in previous_application_df - column name: mainoccupationinc_437A
 # 3.Marital status - found in previous_application_df - column name: familystate_726L
-# 4.Gender 
-# 5.duration of loan 
-# 6.number of people to support 
-# 7.Loan amount
-# 8.existing bank status 
-# 9.Education 
-# 10 exisitig debt amount
+# 4.Gender - found in person_data - column: gender_992L & found in person_data - column: sex_738L
+# 5.Duration of loan - 
+# 6.number of children - found in previous_application_df & person_data - column name: childnum185L, childnum_21L
+# 7.Loan amount - 
+# 8.existing bank status - 
+# 9.Existing debt amount - found in previous_application_df - column name: outstandingdebt_522A
 
 # My goal is to identiy which file holds each of the features and then merge them to create a simple dataset
