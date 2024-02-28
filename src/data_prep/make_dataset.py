12472497591c23fd6dfcd8fb47fe78f43b6ba4c6
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-02-28 09:52:35 +0000

    ML dataset curation ongoing

--- src/data_prep/make_dataset.py
+++ src/data_prep/make_dataset.py
@@ -1,9 +1,13 @@
 #%%
+import pandas as pd
 from src.data_prep import data_preprocess as dp
 
 
 #%%
 def read_parent_data(file_paths:list):
+    '''
+    This function reads the parent data from the file paths and returns a list of dataframes
+    '''
     dataframes = []
     for path in file_paths:
         name = path.split(".")[1] + "_df"
@@ -11,40 +15,43 @@ def read_parent_data(file_paths:list):
         dataframes.append(name)
     return dataframes
 
-base_data, static_data, person_data, previous_application = read_parent_data(["data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_base.parquet",
-                                                                    "data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_static_cb_0.parquet",
-                                                                    "data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_person_1.parquet",
-                                                                    "data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_applprev_1_1.parquet"])
-
-#%%
-
-def create_feature_df(feature_name:str, parentfeature_dict:dict, feature_recency:str, join_method:str, proposed_datatype:dict):
-    n = len(parentfeature_dict)
+def create_feature_df(feature_name:str, parentfeature:dict,
+                      feature_recency:str, proposed_datatype:dict,
+                      join_method:str = None, common_col:str = None) -> pd.DataFrame:
+    '''
+    This function uses the methods in the data_preprocess module to create a feature dataframe.
+    This happens by getting the individual features from the parent dataframes, 
+    merging them into a single dataframe, consolidating the features into a single column and 
+    handling the datatype of the columns in the dataframe. 
+    
+    ** of importance is the parentfeature which is a dictionary with the key as progressive number 
+    in string (1,2,3 etc) and the value as a tuple of the dataframe and the subset column name.**
+    '''
     features = []
-    for key, value in parentfeature_dict.items(): #include enumerate to handle more than one dataframe
-        feature = dp.get_individual_feature(value[0], value[1], feature_recency)
+    for _, (key, value) in enumerate(parentfeature.items()):
+        dataframe, column = value[0], value[1]
+        feature = feature_name + key
+        feature = dp.get_individual_feature(df=dataframe, col_list=column, recency=feature_recency)
         features.append(feature)
-    #features_combined = dp.merge_features_into_df(dfs=features, on_col="case_id", join_type=join_method) #deal with this method to handle more than one dataframe
-    feature_df = dp.create_consolicated_df(df=features[0], new_colname=feature_name)
+    features_combined = dp.merge_features_into_df(dfs=features, on_col=common_col, 
+                                                  join_type=join_method)
+    feature_df = dp.create_consolicated_df(df=features_combined, new_colname=feature_name)
     feature_df = dp.handle_features_datatype(df=feature_df, col_datatype=proposed_datatype)
     return feature_df
 
-dob_df = create_feature_df(feature_name="dateofbirth",
-                           parentfeature_dict={"One":(static_data, ['case_id', "dateofbirth_337D"])},
-                           feature_recency="last",
-                           join_method="outer",
-                           proposed_datatype={"case_id":"integer", "dateofbirth":"date"})
-
-#%%
-
-def create_ml_dataset():
-    pass
-
+def create_ml_dataset(feature_dfs:list, base_df:pd.DataFrame, common_col:str = None) -> pd.DataFrame:
+    '''
+    This function takes all the individual feature dataframes and the base dataframe and creates
+    the final dataset for machine learning.
+    '''
+    ml_df = base_df
+    for df in feature_dfs:
+        ml_df = pd.merge(ml_df, df, on=common_col, how="outer")
+    return ml_df
 
 #%%
 def split_ml_dataset():
     pass
-
 
 #%%
 def save_traindev_dataset():
@@ -53,67 +60,58 @@ def save_traindev_dataset():
 
 def main():
     pass
+#     base_data, static_data, person_data, previous_application = read_parent_data(
+#         ["data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_base.parquet",
+#         "data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_static_cb_0.parquet",
+#         "data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_person_1.parquet",
+#         "data/raw/home-credit-credit-risk-model-stability/parquet_files/train/train_applprev_1_1.parquet"])
+    
+#     dob_df = create_feature_df(feature_name="dateofbirth",
+#                            parentfeature={"One":(static_data, ['case_id', "dateofbirth_337D"]),
+#                                         "Two":(static_data, ['case_id', "dateofbirth_342D"])},
+#                            feature_recency="last",
+#                            common_col="case_id",
+#                            join_method="outer",
+#                            proposed_datatype={"case_id":"integer", "dateofbirth":"date"})
 
+#     gender_df = create_feature_df(feature_name='gender',
+#                               parentfeature={"One":(person_data, ['case_id', "sex_738L"])},
+#                               feature_recency="first",
+#                               join_method="outer",
+#                               proposed_datatype={"case_id":"integer", "gender":"string"})
 
+#     child_df = create_feature_df(feature_name='no_of_children',
+#                              parentfeature={"One":(previous_application, ['case_id', "childnum_21L"]),
+#                                             "Two":(person_data, ['case_id', "childnum_185L"])},
+#                              feature_recency="last",
+#                              join_method="outer",
+#                              proposed_datatype={"case_id":"integer", "no_of_children":"integer"})
+
+#     marital_df = create_feature_df(feature_name='marital_status',
+#                                   parentfeature={"One":(person_data, ['case_id', "familystate_447L"]),
+#                                                  "Two":(previous_application, ['case_id', "familystate_726L"])},
+#                                   feature_recency="last",
+#                                   join_method="outer",
+#                                   proposed_datatype={"case_id":"integer", "marital_status":"string"})
+
+#     inc_df = create_feature_df(feature_name='income',
+#                            parentfeature={"One":(person_data, ['case_id', "mainoccupationinc_384A"])},
+#                            feature_recency="last",
+#                            join_method="outer",
+#                            proposed_datatype={"case_id":"integer", "income":"float"})
+
+#     debt_df = create_feature_df(feature_name='debt',
+#                             parentfeature={"One":(previous_application, ['case_id', "outstandingdebt_522A"])},
+#                             feature_recency="last",
+#                             join_method="outer",
+#                             proposed_datatype={"case_id":"integer", "debt":"float"})
+
+#     credit_df = create_feature_df(feature_name='credit_status',
+#                               parentfeature={"One":(previous_application, ['case_id', "credacc_status_367L"])},
+#                               feature_recency="last",
+#                               join_method="outer",
+#                               proposed_datatype={"case_id":"integer", "credit_status":"string"})
+    
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-# #%%
-# # Tested for the dob feature
-# dob_df1 = dp.get_individual_feature(static_data, ["case_id", "dateofbirth_337D"], "last")
-# # dob_df2 = get_inidivual_feature(static_data_2, ["case_id", "dateofbirth_342D"], "last")
-# # dob_comb = merge_dfs([dob_df1, dob_df2], "case_id", "outer")
-# # dob_df = create_consolicated_df(dob_comb, "dateofbirth")
-# # dob_df = handle_feature_datatype(dob_df, {"case_id":"integer", "dateofbirth":"date"})
-
-# # %%
-# ## Testing the methods on the gender feature - this is just found in one dataframe so no need to merge
-# gender = dp.get_individual_feature(person_data, ["case_id","sex_738L"], "first")
-# # gender_df = create_consolicated_df(gender, "gender")
-# # gender_df = handle_feature_datatype(gender_df, {"case_id":"integer", "gender":"string"})
-
-# #%%
-# child1 = dp.get_individual_feature(previous_application, ["case_id", "childnum_21L"], "last")
-# # child2 = get_individual_feature(person_data, ["case_id", "childnum_185L"], "last")
-# # child_comb = merge_features_into_df([child1, child2], "case_id", "outer")
-# # child_df = create_consolicated_df(child_comb, "no_of_children")
-
-
-# # %%
-# # Deal with getting the marital status from the previous_application_df
-
-# fam1 = get_individual_feature(previous_application_df, ["case_id", "familystate_726L"], "last")
-# fam2 = get_individual_feature(person_data, ["case_id", "familystate_447L"], "last")
-# fam_comb = merge_features_into_df([fam1, fam2], "case_id", "outer")
-# fam_df = create_consolicated_df(fam_comb, "marital_status")
-# fam_df = handle_feature_datatype(fam_df, {"case_id":"integer", "marital_status":"string"})
-
-
-# # %%
-# #revist the consolidation method to handle integer and float properly
-
-# inc = get_individual_feature(person_data, ["case_id", "mainoccupationinc_384A"], "last")
-# inc_df = create_consolicated_df(inc, "main_income")
-# inc_df = handle_feature_datatype(inc_df, {"case_id":"integer", "main_income":"integer"})
-
-# # %%
-# debt = get_individual_feature(previous_application_df, ["case_id", "outstandingdebt_522A"], "last")
-# debt_df = create_consolicated_df(debt, "outstanding_debt")
-
-# #debt_df = handle_feature_datatype(debt_df, {"case_id":"integer", "outstanding_debt":"integer"})
-
-
-# # %%
-# credit = get_individual_feature(previous_application_df, ["case_id", "credacc_status_367L"], "last")
-# credit_df = create_consolicated_df(credit, "credit_status")
-# credit_df = handle_feature_datatype(credit_df, {"case_id":"integer", "credit_status":"string"})
