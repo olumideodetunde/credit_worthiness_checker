a5ba7f9d39186d5f1b9e4b3e8cb066802bbf595d
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-03-05 07:40:26 +0000

    Logging implemented

--- src/data_prep/__init__.py
+++ src/data_prep/__init__.py
@@ -1,14 +1,12 @@
+'''This module implements the logging functionality for the entire project.'''
 import os
 import sys
 import logging
 
-#logging_format = "[%(asctime)s: %(levelname)s: %(module)s: %(messages)s]"
 logging_format = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
-
 log_dir = "logs"
 log_filepath = os.path.join(log_dir, "entire_logs.log")
 os.makedirs(log_dir, exist_ok=True)
-
 logging.basicConfig(
     level = logging.INFO,
     format = logging_format,
@@ -17,6 +15,4 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ]
 )
-
 logger = logging.getLogger("credit_worthiness_logger")
-
