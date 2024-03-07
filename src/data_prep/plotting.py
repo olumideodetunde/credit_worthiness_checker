2f19a90252374973cce7f15507051727c0d2a840
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-03-07 07:41:06 +0000

    Data Visualisation ongoing

--- src/data_prep/plotting.py
+++ src/data_prep/plotting.py
@@ -10,11 +10,11 @@ dev_df = pd.read_parquet("data/output/dev.parquet")
 
 # %%
 # Confirm that all the datatypes of the columns
-def check_dtypes(df):
+def _check_dtypes(df):
     for col in df.columns:
         print(f"{col}: {df[col].dtype}")
 
-check_dtypes(train_df)
+_check_dtypes(train_df)
 
 # %%
 #Noticed that the date column is not in datetime format
@@ -39,7 +39,7 @@ def check_completeness(df:pd.DataFrame) -> None:
     plt.title("Percentage of missing data")
     plt.xticks(rotation=90)
     plt.show()
-    
+
 check_completeness(train_df)
 # %%
 #Summary statistics of the columns depending on the data type
@@ -49,8 +49,38 @@ def get_numeric_summary(df:pd.DataFrame, numeric_cols:list) -> pd.DataFrame:
     num_df = df[numeric_cols]
     x = num_df.describe().T
     return x
-    
+
 a = get_numeric_summary(train_df, ["no_of_children", "debt", "income"])
 a
-    
+
 # %%
+def display_cat_summary(df:pd.DataFrame, cat_cols:list) -> None:
+    cat_df = df[cat_cols]
+    figs, axes = plt.subplots(3,1)
+    
+    for i, column in enumerate(cat_df.columns):
+        counts = cat_df[column].value_counts()
+        counts.plot(
+            kind ="bar",
+            ax = axes.flatten()[i],
+            fontsize = "large"
+        ).set_title(column)
+    figs.set_size_inches(15, 12)
+    #plt.tight_layout()
+    plt.show()
+
+display_cat_summary(train_df, cat_cols=['gender', 'marital_status', 'credit_status'])
+
+# %%
+#Look at date related columns, create new feature - age
+def create_age(df:pd.DataFrame, date_cols:list) -> pd.DataFrame:
+    df["age"] = df[date_cols[0]].dt.year - df[date_cols[1]].dt.year
+    return df
+
+train_df = create_age(train_df, ["date_decision", "dateofbirth"])
+dev_df = create_age(dev_df, ["date_decision", "dateofbirth"])
+
+# %%
+#Exploring relationships between feautures guided by questions/business understanding & target variable
+
+#Does the age affect the 
