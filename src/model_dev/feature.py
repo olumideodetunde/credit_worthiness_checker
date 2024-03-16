dd1983f4313e33b7d796527860b94386429f0c96
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-03-16 11:58:14 +0000

    Feature engineering ongoing

--- src/model_dev/feature.py
+++ src/model_dev/feature.py
@@ -9,7 +9,6 @@ train_df.set_index("case_id", inplace=True)
 
 
 #Filling the gaps, dropping and deriving new features
-
 #%%
 def calculate_age(df:pd.DataFrame):
     df[['date_decision', 'dateofbirth']] = df[['date_decision', 'dateofbirth']].apply(pd.to_datetime)
@@ -50,23 +49,39 @@ def drop_columns(df:pd.DataFrame, cols:list):
 train_df = drop_columns(train_df, ['credit_status', "debt"])
 
 #%%
-def aggregate_marital_status(df:pd.DataFrame):
-    #rename the none values to not disclosed
-    df['marital_status'] = df['marital_status'].fillna('not disclosed')
+#marital status catgories is too noisy, I will group them into 5 categories
+#grouping list  = ['Married', 'Single', 'Divorced', 'Widowed',civil_partnership, 'Not disclosed']
+grouping_dict = {
+                 'Married': ['Married','Marriedmarried','Singlemarried','Widowedmarried','Divorcedmarried','Living_With_Partnermarried'], 
+                 'Single':  ['Single', 'Singlesingle','Divorcedsingle','Widowedsingle','Marriedsingle','Living_With_Partnersingle'],
+                 'Divorced': ['Divorced', 'Singledivorced', 'Marrieddivorced', 'Divorceddivorced', 'Widoweddivorced', 'Living_With_Partnerdivorced'],
+                 'Widowed': ['Widowed', 'Marriedwidowed', 'Singlewidowed', 'Divorcedwidowed', 'Widowedwidowed', 'Living_With_Partnerwidowed'],
+                 'Not Supplying': ['Not Disclosed'],
+                 'civil_partnership': ['Living_With_Partner','Singleliving_With_Partner','Marriedliving_With_Partner', 'Living_With_Partnerliving_With_Partner', 'Widowedliving_With_Partner', 'Divorcedliving_With_Partner']
+}
+
+def group_marital_status(df:pd.DataFrame, grouping_dict:dict):
+    df['marital_status'] = df['marital_status'].fillna('Not disclosed')
+    df['marital_status'] = df['marital_status'].str.title()
+    df['marital_status_new'] = df['marital_status']
+    for group, values in grouping_dict.items():
+        df.loc[df['marital_status'].isin(values), 'marital_status_new'] = group
     return df
 
-train_df = aggregate_marital_status(train_df)
+train_df = group_marital_status(train_df, grouping_dict)  
 
 # %%
-#testing dropping all the rows with no age
-y = train_df.copy()
-y.dropna(subset=['age'], inplace=True)
+def drop_na_rows(df:pd.DataFrame, cols:str):
+    return df.dropna(subset=cols)
+
+train_df = drop_na_rows(train_df, 'age')
 
 #%%
-
 # Next step is transform the categorical and numerical columns to be ml ready
-def one_hot_encode(df:pd.DataFrame):
-    df = df.select_dtypes(include=['object'])
+def one_hot_encode(df:pd.DataFrame, cat_col_list:list) -> pd.DataFrame:
+    '''This function transforms the categorical columns to be ml ready'''
+    # df = df.select_dtypes(include=['object'])
+    df = df[cat_col_list]
     encoder = OneHotEncoder(sparse_output=False, handle_unknown='error')
     cat_encoded = encoder.fit_transform(df)
     cat_columns = []
@@ -76,7 +91,7 @@ def one_hot_encode(df:pd.DataFrame):
     cat_df = pd.DataFrame(cat_encoded, columns=cat_columns)
     return cat_df
 
-transformed_cat_df = one_hot_encode(train_df)
-
+transformed_cat_df = one_hot_encode(train_df, ['time_of_year', 'family_size', 'marital_status_new'])
 
 # %%
+# Next step is to transform each numerica feature depending on my intuition - this will be used in training to obtain and see the effect of numeric features altered
