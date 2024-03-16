2fa05bd60450611d048fb3f4365b31f1e22ccaae
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-03-16 16:12:44 +0000

    Feature Engineering v1 roughly complete - to be cleaned up and rewritten

--- src/model_dev/feature.py
+++ src/model_dev/feature.py
@@ -4,8 +4,9 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 #%%
-train_df = pd.read_parquet("artifacts/data_prep/output/train.parquet")
-train_df.set_index("case_id", inplace=True)
+raw_df = pd.read_parquet("artifacts/data_prep/output/train.parquet")
+raw_reset_df= raw_df.copy().reset_index(drop=True)
+train_df = raw_reset_df.copy()
 
 
 #Filling the gaps, dropping and deriving new features
@@ -14,10 +15,9 @@ def calculate_age(df:pd.DataFrame):
     df[['date_decision', 'dateofbirth']] = df[['date_decision', 'dateofbirth']].apply(pd.to_datetime)
     df['age'] = (df['date_decision'] - df['dateofbirth']).dt.days // 365
     return df
-
 train_df = calculate_age(train_df)
-# %%
 
+# %%
 def assign_time_of_year(df:pd.DataFrame):
     df['time_of_year'] = df['date_decision'].dt.month.apply(lambda x: 
         'winter' if x in [12, 1, 2] else 'spring' 
@@ -27,19 +27,16 @@ def assign_time_of_year(df:pd.DataFrame):
 
 train_df = assign_time_of_year(train_df)
 
-
 # %%
 def impute_no_of_children(df:pd.DataFrame):
     df['no_of_children'] = df['no_of_children'].fillna(0)
     return df
-
 train_df = impute_no_of_children(train_df)
 
 #%%
 def bin_family_size(df:pd.DataFrame):
     df['family_size'] = df['no_of_children'].apply(lambda x: 'single' if x == 0 else 'small' if x < 2 else 'large')
     return df
-
 train_df = bin_family_size(train_df)
 
 # %%
@@ -72,7 +69,7 @@ train_df = group_marital_status(train_df, grouping_dict)
 
 # %%
 def drop_na_rows(df:pd.DataFrame, cols:str):
-    return df.dropna(subset=cols)
+    return df.dropna(subset=cols).reset_index(drop=True)
 
 train_df = drop_na_rows(train_df, 'age')
 
@@ -94,17 +91,17 @@ train_df = bin_feature(train_df, col='income', col_bin=[0, 25000, 50000, 200000]
 def one_hot_encode(df:pd.DataFrame, cat_col_list:list) -> pd.DataFrame:
     '''This function transforms the categorical columns to be ml ready'''
     # df = df.select_dtypes(include=['object'])
-    df = df[cat_col_list]
+    cat_df = df[cat_col_list]
     encoder = OneHotEncoder(sparse_output=False, handle_unknown='error')
-    cat_encoded = encoder.fit_transform(df)
+    cat_encoded = encoder.fit_transform(cat_df)
     cat_columns = []
-    for i, col in enumerate(df.columns):
+    for i, col in enumerate(cat_df.columns):
         for cat in encoder.categories_[i]:
             cat_columns.append(f"{col}_{cat}")
     cat_df = pd.DataFrame(cat_encoded, columns=cat_columns)
     return cat_df
 
-x = one_hot_encode(train_df, ['time_of_year', 'family_size', 'marital_status_new', 'age_binned', 'income_binned'])
+x = one_hot_encode(train_df, ['time_of_year','gender', 'family_size', 'marital_status_new', 'age_binned', 'income_binned'])
 
 #%%
 #Log transform the income 
@@ -115,15 +112,23 @@ train_df = log_transform(train_df, 'income')
 
 #%%
 #Standardise numerical features
-def standardise(df: pd.DataFrame, cols: list):
+def standardise_feature(df: pd.DataFrame, cols: list):
+    #Handle standrisation for train, dev and test with scaler
     scaler = StandardScaler()
     for col in cols:
         col_data = df[[col]]  # Extract column data as DataFrame
         df[str(col)+'_standardised'] = scaler.fit_transform(col_data)  # Fit and transform
     return df
-train_df = standardise(train_df, ['age', 'income'])
+train_df = standardise_feature(train_df, ['age', 'income'])
 
-# %%
-# Write a normalised function
-#Sort the joining of categorical and numerical features to create the ml ready data
-
+#%%
+#Create the ml reasy dataframe with raw and transformed features
+def create_ml_ready_df(dfs:list):
+    df = dfs[0].join(dfs[1]).set_index('case_id')
+    col_types = df.dtypes
+    obj_col = col_types[col_types == 'object'].index.tolist()
+    num_col = col_types[col_types != 'object'].index.tolist()
+    sorted_cols = obj_col + num_col
+    df = df[sorted_cols]
+    return df
+ml_df = create_ml_ready_df([train_df, x])
