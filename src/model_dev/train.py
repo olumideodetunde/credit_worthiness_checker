cf7a4fe0a05d449716293097f017d5863e07610c
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-03-28 09:03:53 +0000

    training pipeline strategy identified

---
+++ src/model_dev/train.py
@@ -0,0 +1,15 @@
+#Establish a baseline
+
+#Implement experiment tracking and logging
+
+#Set up to test different models
+
+#Set up to test different hyperparameters
+
+#Set up to test different features
+
+#Log each results with eval.py
+
+#Identitfy the best model
+
+#Export the best model
