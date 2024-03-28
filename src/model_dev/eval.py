b434ab3d7e6ba50ef3d6e39d65fc58e3a585a0f8
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-03-28 07:49:29 +0000

    Figuring evaluation metrics

---
+++ src/model_dev/eval.py
@@ -0,0 +1,6 @@
+#The goal is to come up with one whole metric that ties neatly into the business metric.
+
+#The appropriate threshold for this is precision-recall tradeoff
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_recall_curve
