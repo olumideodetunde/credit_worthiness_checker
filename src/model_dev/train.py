74af897e8aac8cf6432ed025ebde4f05cedf8108
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-04-01 14:58:05 +0100

    Train pipeline run successfully - now modelling for improved metrics

--- src/model_dev/train.py
+++ src/model_dev/train.py
@@ -1,7 +1,6 @@
 #%%
-import pandas as pd
-import numpy as np
 import mlflow
+import pandas as pd
 from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
@@ -17,91 +16,96 @@ class ModelTrainer:
         self.dev_data = None
         self.model = None
         self.metrics = None
-        self.run_id = None
 
     def _split_data_into_features_target(self, data):
         features = data.drop(columns=["target"])
         target = data["target"]
         return features, target
 
-    def initialise_training(self) -> None:
-        mlflow.set_experiment("CreditWorthinessModelTraining")
-        mlflow.start_run()
-        self.run_id = mlflow.active_run().info.run_id
-        return None
-   
+    def _make_prediction(self, data) -> tuple:
+        features, target = self._split_data_into_features_target(data)
+        predict = self.model.predict(features.iloc[:, 15:])
+        y_score = self.model.predict_proba(features.iloc[:, 15:])[:,1]
+        return predict, y_score
+
     def load_data(self, train_name:str, dev_name:str) -> pd.DataFrame:
         try:
-            with mlflow.start_run(run_id=self.run_id):
-                self.train_data = pd.read_parquet(self.input_path + '/' + train_name)
-                self.dev_data = pd.read_parquet(self.input_path + '/' + dev_name)
-                mlflow.log_param("train_data", self.train_data.describe())
-                mlflow.log_param("dev_data", self.dev_data.describe())
+            self.train_data = pd.read_parquet(self.input_path + '/' + train_name)
+            self.dev_data = pd.read_parquet(self.input_path + '/' + dev_name)
             return self.train_data, self.dev_data
         except FileNotFoundError:
             raise FileNotFoundError("File not found")
 
     def select_model(self, algorithm:str, hyperparameters:dict=None) -> object:
-        with mlflow.start_run(run_id=self.run_id):
-            if algorithm == "RandomForest":
-                self.model = RandomForestClassifier(**hyperparameters)
-            elif algorithm == "LogisticRegression":
-                self.model = LogisticRegression(**hyperparameters)
-            elif algorithm == "Baseline":
-                self.model = DummyClassifier(strategy="most_frequent")
-            else:
-                raise ValueError("Invalid algorithm")
-            mlflow.log_param("algorithm", algorithm)
-            mlflow.log_param("hyperparameters", hyperparameters)
+        if algorithm == "RandomForest":
+            self.model = RandomForestClassifier()
+        elif algorithm == "LogisticRegression":
+            self.model = LogisticRegression()
+        elif algorithm == "Baseline":
+            self.model = DummyClassifier(strategy="most_frequent")
+        else:
+            raise ValueError("Invalid algorithm")
         return self.model
 
     def train_model(self) -> object:
-        with mlflow.start_run(run_id=self.run_id):
-            train_features, train_target = self._split_data_into_features_target(self.train_data)
-            self.model.fit(train_features, train_target)
-            mlflow.sklearn.log_model(self.model, "model")
+        train_features, train_target = self._split_data_into_features_target(self.train_data)
+        self.model.fit(train_features.iloc[:,15:], train_target)
         return self.model
 
-    def predict(self, data) -> tuple:
-        with mlflow.start_run(run_id=self.run_id):
-            features, target = self._split_data_into_features_target(data)
-            predict = self.model.predict(features)
-            y_score = self.model.predict_proba(features)[:,1]
-        return predict, y_score
-
     def evaluate_model(self) -> dict:
-        with mlflow.start_run(run_id=self.run_id):
-            predict, y_score = self.predict(self.dev_data)
-            _, dev_target = self._split_data_into_features_target(self.dev_data)
-            accuracy = accuracy_score(dev_target, predict)
-            precision = precision_score(dev_target, predict)
-            recall = recall_score(dev_target, predict)
-            auc_pr = eval_with_auc_and_pr_curve(dev_target, y_score)
-            auc_pr2 = eval_with_average_precision_score(dev_target, y_score)
-            f_beta_score = eval_with_f_beta_score(dev_target, predict)
-            self.metrics = {
-                "accuracy": accuracy,
-                "precision": precision,
-                "recall": recall,
-                "auc_pr": auc_pr,
-                "auc_pr2": auc_pr2,
-                "f_beta_score": f_beta_score
-            }
-            mlflow.log_metrics(self.metrics)
+        predict, y_score = self._make_prediction(self.dev_data)
+        _, dev_target = self._split_data_into_features_target(self.dev_data)
+        accuracy = accuracy_score(dev_target, predict)
+        precision = precision_score(dev_target, predict)
+        recall = recall_score(dev_target, predict)
+        auc_pr = eval_with_auc_and_pr_curve(dev_target, y_score)
+        auc_pr2 = eval_with_average_precision_score(dev_target, y_score)
+        f_beta_score = eval_with_f_beta_score(dev_target, predict)
+        self.metrics = {
+            "accuracy": accuracy,
+            "precision": precision,
+            "recall": recall,
+            "auc_pr": auc_pr,
+            "auc_pr2": auc_pr2,
+            "f_beta_score": f_beta_score
+        }
         return self.metrics
 
     def end_training(self) -> None:
         mlflow.end_run()
         return None
 
-#%%
-if __name__ == "__main__":
+def main():
+    mlflow.set_experiment("CreditWorthinessTraining")
+    with mlflow.start_run():
         trainer = ModelTrainer(input_path="artifacts/data_prep/output")
-        trainer.initialise_training()
-        trainer.load_data("train_data.parquet", "dev_data.parquet")
-        trainer.select_model("RandomForest")
+        trainer.load_data("ml_train.parquet", "ml_dev.parquet")
+        mlflow.log_param("train_data", trainer.train_data.describe())
+        mlflow.log_param("dev_data", trainer.dev_data.describe())
+        trainer.select_model("RandomForestwq")
+        mlflow.log_param("algorithm", "RandomForest")
+        mlflow.log_param("hyperparameters", trainer.model.get_params())
         trainer.train_model()
-        trainer.evaluate_model()
+        mlflow.log_param("model", trainer.model)
+        metrics = trainer.evaluate_model()
+        mlflow.log_metrics(metrics)
         trainer.end_training()
 
+#%%
+if __name__ == "__main__":
+    main()
+    # mlflow.set_experiment("CreditWorthinessTraining")
+    # with mlflow.start_run():
+    #     trainer = ModelTrainer(input_path="artifacts/data_prep/output")
+    #     trainer.load_data("ml_train.parquet", "ml_dev.parquet")
+    #     mlflow.log_param("train_data", trainer.train_data.describe())
+    #     mlflow.log_param("dev_data", trainer.dev_data.describe())
+    #     trainer.select_model("RandomForest")
+    #     mlflow.log_param("algorithm", "RandomForest")
+    #     mlflow.log_param("hyperparameters", trainer.model.get_params())
+    #     trainer.train_model()
+    #     mlflow.log_param("model", trainer.model)
+    #     metrics = trainer.evaluate_model()
+    #     mlflow.log_metrics(metrics)
+    #     trainer.end_training()
 # %%
