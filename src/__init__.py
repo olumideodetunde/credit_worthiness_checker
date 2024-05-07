02c81b924ccd6f41c5af8d618d68e26a2e676c13
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-05-07 17:00:45 +0100

    source init created

--- src/__init__.py
+++ src/__init__.py
@@ -3,19 +3,44 @@ import os
 import sys
 import logging
 
-logging_format = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
-log_dir = "logs"
-log_filepath = os.path.join(log_dir, "entire_logs.log")
-os.makedirs(log_dir, exist_ok=True)
-logging.basicConfig(
-    level = logging.INFO,
-    format = logging_format,
-    handlers = [
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
-    ]
-)
-logger = logging.getLogger("credit_worthiness_logger")
+# logging_format = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+# log_dir = "logs"
+# log_filepath = os.path.join(log_dir, "entire_logs.log")
+# os.makedirs(log_dir, exist_ok=True)
+# logging.basicConfig(
+#     level = logging.INFO,
+#     format = logging_format,
+#     handlers = [
+#         logging.FileHandler(log_filepath),
+#         logging.StreamHandler(sys.stdout)
+#     ]
+# )
+# logger = logging.getLogger("credit_worthiness_logger")
 
+def get_logger():
+    logging_format = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+    log_dir = "logs"
+    log_filepath = os.path.join(log_dir, "entire_logs.log")
+    os.makedirs(log_dir, exist_ok=True)
+    
+    logger = logging.getLogger("credit_worthiness_logger")
+    logger.setLevel(logging.INFO)
 
-PROJECT_ROOT = "/Users/olumide/Documents/Self Improvement - ML/MLapps/credit_worthiness_checker" 
+    # Create formatter
+    formatter = logging.Formatter(logging_format)
+
+    # Create file handler and set level to info
+    file_handler = logging.FileHandler(log_filepath)
+    file_handler.setFormatter(formatter)
+
+    # Create stream handler
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+
+    # Add handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+src_logger = get_logger()
