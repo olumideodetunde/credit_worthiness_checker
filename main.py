2805da523742ca2e34909081583a586f423dfd16
Olumide Odetunde <odetundeolumide95@gmail.com>
2024-05-07 16:42:45 +0100

    src main comeplete flow updated - to be reviwed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 '''This module is the main script for preparing ml dataset, visualising the data,
 training the model and exporting the model'''
-from src import logger
-from src.data_prep.make_dataset import main as data_prep_main
-from src.data_prep.plotting import main as data_plot_main
+from src import src_logger as logger
+from src.data_preprocessing import main as data_prep_main
+from src.plotting import main as data_plot_main
+from src.feature_engineering import main as feature_eng_main
+from src.train import main as train_main
 
 PROJECT_STAGE = "Dataset Preparation"
 try:
@@ -32,3 +34,28 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+PROJECT_STAGE = "Feature Engineering"
+try:
+    logger.info(">>>>>> Started <<<<<< %s", PROJECT_STAGE)
+    INPUT_LIST = [
+    "artifacts/data_prep/output/train.parquet",
+    "artifacts/data_prep/output/dev.parquet"
+    ]
+    OUTPUT_DIR = "artifacts/feature_eng/output"
+    feature_eng_main(input_paths=INPUT_LIST, output_path=OUTPUT_DIR)
+    logger.info(">>>>>> Ended <<<<<< %s", PROJECT_STAGE)
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+PROJECT_STAGE = "Model Training and Export"
+try:
+    logger.info(">>>>>> Started <<<<<< %s", PROJECT_STAGE)
+    INPUT_LIST = [
+    "artifacts/feature_eng/output/ml_train.parquet",
+    "artifacts/feature_eng/output/ml_dev.parquet"
+    ]
+    OUTPUT_DIR = "artifacts/model_training/output"
+    train_main(input_paths=INPUT_LIST, output_path=OUTPUT_DIR)
+    logger.info(">>>>>> Ended <<<<<< %s", PROJECT_STAGE)
